060ce6a22664b1b6eb0df98f6b0f65f24e0f5855
Trottie McQueen <mcqueen.trottie007@gmail.com>
2023-09-29 19:25:36 -0400

    updated javascript

---
+++ starter-code/script.js
@@ -0,0 +1,53 @@
+
+// DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('result');
+const symbolsEl = document.getElementById('result');
+const generateEl = document.getElementById('result');
+const clipboardEl = document.getElementById('result');
+
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+}
+
+// Generate event listen
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasHigher = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(hasLower, hasHigher, hasNumber, hasSymbol, length);
+});
+
+//Generate password function
+
+
+// Generator functions - http://www.net-comber.com/charset.html
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*()_+=-';
+  return symbols[Math.floor(Math.random() = symbols.length)];
+}
+
+console.log(getRandomNumber());
